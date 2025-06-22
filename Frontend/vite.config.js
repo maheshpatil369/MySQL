3f06344fc4a5fd36e851851bcf35d92371ba9bbb
maheshpatil369 <maheshpatilmp937@gmail.com>
2025-06-22 22:50:45 +0530

    Update this for deploy to vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Ensures proper routing on Vercel
+  base: '/', 
   plugins: [react()],
+  build: {
+    outDir: 'dist', 
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
